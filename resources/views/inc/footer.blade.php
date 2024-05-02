d5173b51c3d22ddc53b0874f7b40b41a660e24ba
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-05-02 12:06:16 +0300

    footer change

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -84,7 +84,14 @@
                         <div class="icon"><i class="far fa-envelope"></i></div>
                         <div class="text"><span>
                                 <p>info@candytoursafari.co.tz</p>
-                                <i class="far fa-envelope"></i><p>sabinmorris8@gmail.com</p>
+                                
+                            </span></div>
+                    </div>
+
+                    <div class="footer-address-item">
+                        <div class="icon"><i class="far fa-envelope"></i></div>
+                        <div class="text"><span>
+                                <p>sabinmorris8@gmail.com</p>
                             </span></div>
                     </div>
 
