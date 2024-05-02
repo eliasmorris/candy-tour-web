716780113ba2b98af999d61e4334112852d16676
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-05-02 18:56:56 +0300

    hcontact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -87,8 +87,7 @@
                             <h4>Address in Map</h4>
                         </div>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4804.156059369445!2d39.234612575782094!3d-6.089812493896439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185cd32205083a05%3A0x76c01d569ad012b6!2sTO%20THE%20HILL%20KIDICHI%20SPICE%20FARMS!5e1!3m2!1sen!2stz!4v1714641308469!5m2!1sen!2stz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3313.3833161665298!2d-118.03745848530627!3d33.85401093559897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd2c6c97f8f3ed%3A0x47b1bde165dcc056!2sOak+Dr%2C+La+Palma%2C+CA+90623%2C+USA!5e0!3m2!1sen!2sbd!4v1544238752504" width="600" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe> -->
-                    </div>
+                        
                 </div>
             </div>
         </div>
