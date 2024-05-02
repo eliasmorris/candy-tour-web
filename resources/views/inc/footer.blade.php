ab0f5f52e77f15b1e23e2c64c95c9edf6e0a9019
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-05-02 18:49:11 +0300

    footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -161,7 +161,7 @@
 </script>
 
 <div class="live-chat-container">
-    <!--Start of Tawk.to Script-->
+    <!--Start of Tawk.to Scripts-->
     <script type="text/javascript">
         var Tawk_API = Tawk_API || {},
             Tawk_LoadStart = new Date();
