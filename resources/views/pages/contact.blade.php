fd5f6f9731a48d85cb8ca314e8c59bf04561a00a
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-05-03 09:06:57 +0300

    contact form update

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -23,7 +23,7 @@
                         <div class="headstyle">
                             <h4>Contact Form</h4>
                         </div>
-                        <form action="#" method="post">
+                        <form action="#" method="post" enctype="multipart/form-data">
                             <input type="hidden" name="_token" value="Dccr0BuK08xOGx5d6rAYgp05Bhiq8HnhPtgxLvxk">                            <div class="form-row">
                                 <div class="form-group mb-3">
                                     <input type="text" class="form-control" placeholder="Name" name="visitor_name">
