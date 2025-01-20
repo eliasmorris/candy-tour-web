c0c1c3142775b675c59cca18200b08055ac624f5
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-01-20 14:56:15 +0300

    some changejk

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit57da29b01a8b2db9e33417259f8dbca1
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit57da29b01a8b2db9e33417259f8dbca1', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit57da29b01a8b2db9e33417259f8dbca1', 'loadClassLoader'));
